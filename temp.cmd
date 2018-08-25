548aeb423ba0e55b5dc96ffee06f1ca87f7baec4
disaderp <disaderp@users.noreply.github.com>
2018-08-25 22:37:55 +0200

    Update temp.cmd

--- temp.cmd
+++ temp.cmd
@@ -4,3 +4,5 @@ mount -t vfat /dev/mapper/luks-sda-part1 /root/enc
 umount /root/enc
 dmsetup remove luks-sda1
 cryptsetup luksClose luks-sda
+modprobe -r g_serial
+modprobe g_mass_storage file=/dev/mapper/luks-sda
