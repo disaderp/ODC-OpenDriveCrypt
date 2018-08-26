96a360f15466488a5bc82e7b5273078f52c3cc68
Disa <dis@mail.rotf.pl>
2018-08-27 01:46:40 +0200

    lfcr -> lf

--- ODC-setup.sh
+++ ODC-setup.sh
@@ -8,9 +8,9 @@ apt-get install cryptsetup -y
 echo "Listing avaliable block devices"
 #block devices - disk - not ram - not mmc(boot device)
 lsblk | grep disk | grep -v ram | grep -v mmc
-read -p 'Enter device from the list: ' devname
+read -p -r 'Enter device from the list: ' devname
 
-echo THE DATA ON /dev/$devname WILL BE ERASED
+echo "THE DATA ON /dev/$devname WILL BE ERASED"
 read -n 1 -s -r -p "Press any key to continue..."
 
 #disable startup g_serial
@@ -20,15 +20,16 @@ echo 0 > /etc/modules
 echo "0" > /root/setup
 
 #save devname
-echo $devname > /root/devid
+echo "/dev/$devname" > /root/devid
 
 #add startup script
+mv -f ./ODC-startup.sh /root/ODC-startup.sh
 echo "$(echo '/root/ODC-startup.sh\n' | cat - /etc/rc.local)" > /etc/rc.local
 
 echo "Notice: if you are just doing a reinstall and have a working LUKS drive"
-echo "Enter this command next - echo "1" > /root/setup"
+echo "Enter this command next - echo \"1\" > /root/setup"
 echo "Configuration done. Make sure to plug external keyboard to USB hub"
-echo "The device will now boot in headless mode with disabled USBserial interface"1
+echo "The device will now boot in headless mode with disabled USBserial interface"
 
 #TODO: disable all internet interfaces permanently
 
--- ODC-startup.sh
+++ ODC-startup.sh
@@ -10,23 +10,23 @@ SETUP=$( cat /root/setup )
 #TODO
 
 
-if [ $SETUP -eq 0 ] then
+if [ $SETUP -eq 0 ] ; then
 	#signal password prompt
-	echo "default-on" > /sys/class/leds/orangepi\:red\:status/trigger
+	echo "default-on" > /sys/class/leds/orangepi:red:status/trigger
 	
 	#setup luks container
 	cryptsetup luksFormat $DEVICE
 	
 	#error chk
-	if [ $? -ne 0 ] then
+	if [ $? -ne 0 ] ; then
 		#signal error
-		echo "heartbeat" > /sys/class/leds/orangepi\:green\:pwr/trigger
+		echo "heartbeat" > /sys/class/leds/orangepi:green:pwr/trigger
 		#need reboot/reinstall
 		exit 1
 	fi
 	
 	#signal password prompt-off
-	echo "default-off" > /sys/class/leds/orangepi\:red\:status/trigger
+	echo "default-off" > /sys/class/leds/orangepi:red:status/trigger
 	
 	#write
 	echo "1" > /root/setup
@@ -36,21 +36,21 @@ fi
 modprobe -r g_serial
 
 #signal password prompt
-echo "default-on" > /sys/class/leds/orangepi\:red\:status/trigger
+echo "default-on" > /sys/class/leds/orangepi:red:status/trigger
 
 #password prompt
 cryptsetup luksOpen $DEVICE luksdev
 
 #error chk
-if [ $? -ne 0 ] then
+if [ $? -ne 0 ] ; then
 	#signal error
-	echo "heartbeat" > /sys/class/leds/orangepi\:green\:pwr/trigger
+	echo "heartbeat" > /sys/class/leds/orangepi:green:pwr/trigger
 	#need reboot/reinstall
 	exit 1
 fi
 
 #signal password prompt
-echo "default-off" > /sys/class/leds/orangepi\:red\:status/trigger
+echo "default-off" > /sys/class/leds/orangepi:red:status/trigger
 
 #enable g_mass_storage
 modprobe g_mass_storage file=/dev/mapper/luksdev
