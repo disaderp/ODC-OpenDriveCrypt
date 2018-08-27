885c71a0811cc8f06cdbc49f3bd5b9f7f7b7fdf4
Disa <dis@mail.rotf.pl>
2018-08-27 02:06:33 +0200

    setup fixes

--- ODC-setup.sh
+++ ODC-setup.sh
@@ -8,13 +8,13 @@ apt-get install cryptsetup -y
 echo "Listing avaliable block devices"
 #block devices - disk - not ram - not mmc(boot device)
 lsblk | grep disk | grep -v ram | grep -v mmc
-read -p -r 'Enter device from the list: ' devname
+read -r -p "Enter device from the list: " devname
 
 echo "THE DATA ON /dev/$devname WILL BE ERASED"
 read -n 1 -s -r -p "Press any key to continue..."
 
 #disable startup g_serial
-echo 0 > /etc/modules
+> /etc/modules
 
 #run config on next startup
 echo "0" > /root/setup
@@ -24,9 +24,10 @@ echo "/dev/$devname" > /root/devid
 
 #add startup script
 mv -f ./ODC-startup.sh /root/ODC-startup.sh
-echo "$(echo '/root/ODC-startup.sh\n' | cat - /etc/rc.local)" > /etc/rc.local
+chmod +x /root/ODC-startup.sh
+echo "$(echo '/root/ODC-startup.sh' | cat - /etc/rc.local)" > /etc/rc.local
 
-echo "Notice: if you are just doing a reinstall and have a working LUKS drive"
+echo -e "\n\nNotice: if you are just doing a reinstall and have a working LUKS drive"
 echo "Enter this command next - echo \"1\" > /root/setup"
 echo "Configuration done. Make sure to plug external keyboard to USB hub"
 echo "The device will now boot in headless mode with disabled USBserial interface"
