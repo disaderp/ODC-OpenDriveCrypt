3ba50f1e9b5691261652a9a04ecbc9ac5978fe97
disaderp <disaderp@users.noreply.github.com>
2018-08-29 03:41:53 +0200

    Delete temp.cmd

--- temp.cmd
+++
@@ -1,8 +0,0 @@
-cryptsetup luksOpen /dev/sda luks-sda
-kpartx -av /dev/mapper/luks-sda
-mount -t vfat /dev/mapper/luks-sda-part1 /root/enc
-umount /root/enc
-dmsetup remove luks-sda1
-cryptsetup luksClose luks-sda
-modprobe -r g_serial
-modprobe g_mass_storage file=/dev/mapper/luks-sda
